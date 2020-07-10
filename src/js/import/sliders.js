ae0e8651bcadd4f4b61067a6538f468fc94fdf63
awam-it <awam-it@users.noreply.github.com>
2020-07-10 15:11:10 +0300

    Main slider js update

--- src/js/import/sliders.js
+++ src/js/import/sliders.js
@@ -6,24 +6,6 @@
 
 $(document).ready(function() {
 
-    /* slider */
-    if ($(".main-slider__block")[0]){
-        var slider = tns({
-            container: '.main-slider__block',
-            items:1,
-            nav: true,
-            navPosition: 'bottom',
-            controls: false,
-            gutter:0,
-            loop:true,
-            responsive:{
-                750:{
-                    controls: true,
-                },
-            }
-        });
-    }
-
     /* two rows */
     if ($(".slider-two-rows")[0]){
     var slider = tns({
