a3c6bae49e994989afe380e111609beeb8e0ae0f
awam-it <awam-it@users.noreply.github.com>
2020-07-10 05:06:59 +0300

    Category page added

--- src/js/import/sliders.js
+++ src/js/import/sliders.js
@@ -167,38 +167,64 @@ var slider = tns({
     }
 });
 }
-});
-/*
-$('.slider-cards__slider-new').owlCarousel({
-    loop:true,
-    margin:16,
-    nav:false,
-    dots:true,
-    items:6,
-    responsive:{
-        320:{
+    /* Categories tranding */
+    if ($(".trending-slider")[0]){
+        var slider = tns({
+            container: '.trending-slider',
             items:1,
-            nav:false,
-            dots:true,
-        },
-        750:{
-            items:3,
-            nav:false,
-            dots:true,
-        },
-        1024:{
-            items:4,
-        },
-        1200:{
-            items:4,
-            nav:true, dots:false
-        },
-        1420:{
-            items:6,
-            nav:false, dots:true,
-        },
-        1540:{
-            nav:true, dots:false,
-        },
+            nav: true,
+            navPosition: 'bottom',
+            controls: false,
+            gutter:0,
+            loop:true,
+            responsive:{
+                750:{
+
+                },
+            }
+        });
     }
-});*/
+
+    /* Promo */
+    if ($(".categories-promo")[0]){
+        const tnsCarousel = document.querySelectorAll('.categories-promo');
+        tnsCarousel.forEach(slider => {
+            const tnsSlider = tns({
+                container: slider,
+                items:4,
+                navPosition: 'bottom',
+                gutter:16,
+                onInit: true,
+                loop:false,
+                controls: false,
+                nav: true,
+                responsive:{
+                    320:{
+                        items:1,
+                        loop:false,
+                    },
+                    750:{
+                        items:3,
+                        loop:false,
+                    },
+                    1024:{
+                        items:4,
+                        loop:true,
+                    },
+                    1200:{
+                        items:4,
+                    },
+                    1420:{
+                        items:6,
+                    },
+                    1540:{
+
+                    },
+                }
+            });
+        });
+    }
+
+
+
+});
